4d47c78823ceb5241965dd59b1f82db2d9eac476
chentao <chen.tao@microcardio.com>
2017-04-24 17:23:28 +0800

    1 整理 设置 页面图片 删除无用 代码

--- app/src/main/java/com/example/practice/app/home/MeFragment.java
+++ app/src/main/java/com/example/practice/app/home/MeFragment.java
@@ -1,7 +1,5 @@
 package com.example.practice.app.home;
 
-import android.accounts.AccountManager;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,11 +10,8 @@ import android.widget.TextView;
 
 import com.example.practice.R;
 import com.example.practice.app.setting.accountManage.AccountManageActivity;
-import com.example.practice.app.setting.nodistrub.NoDistrubActivity;
 import com.example.practice.app.setting.normal.NormalActivity;
 import com.example.practice.app.setting.privateandsafe.PrivateAndSafe;
-import com.example.practice.app.setting.privateandsafe.gesturelock.CreateGestureLockActivity;
-import com.example.practice.app.setting.privateandsafe.gesturelock.GuestureLockActivity;
 import com.example.practice.app.setting.remind.RemindActivity;
 import com.example.practice.app.setting.statistics.StatisticalFlowActivity;
 import com.example.practice.app.setting.user.UserEditActivity;
@@ -43,25 +38,21 @@ public class MeFragment extends Fragment {
         title.setText(R.string.setting);
         //1.账户管理
         initAccount_manage();
-        //1 个人信息
+        //2 个人信息
         initPersonal_info();
-        //2. 消息提醒
+        //3. 消息提醒
         initRemind();
-        //3.聊天设置
-        initChatSetting();
-        //4.勿扰模式
-        initNoDistrub();
-        //5.我的隐私
+        //4.我的隐私
         initPrivate();
-        //6.通用设置
+        //5.通用设置
         initPublic();
-        //7.流量统计
+        //6.流量统计
         initTrafficStatistics();
         return view;
     }
     private void initAccount_manage() {
         MeItemView miv_personal_info = (MeItemView) view.findViewById(R.id.miv_account_manage);
-        miv_personal_info.setImage(R.mipmap.icon);
+        miv_personal_info.setImage(R.drawable.usermanage);
         miv_personal_info.setTitle("账户管理");
         miv_personal_info.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,7 +63,7 @@ public class MeFragment extends Fragment {
     }
     private void initPersonal_info() {
         MeItemView miv_personal_info = (MeItemView) view.findViewById(R.id.miv_personal_info);
-        miv_personal_info.setImage(R.mipmap.icon);
+        miv_personal_info.setImage(R.drawable.editinfo);
         miv_personal_info.setTitle("编辑资料");
         miv_personal_info.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,7 +74,7 @@ public class MeFragment extends Fragment {
     }
     private void initRemind() {
         MeItemView miv_personal_info = (MeItemView) view.findViewById(R.id.miv_remind);
-        miv_personal_info.setImage(R.mipmap.pictures);
+        miv_personal_info.setImage(R.drawable.remind);
         miv_personal_info.setTitle("消息提醒");
         miv_personal_info.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,31 +83,9 @@ public class MeFragment extends Fragment {
             }
         });
     }
-    private void initChatSetting() {
-        MeItemView miv_personal_info = (MeItemView) view.findViewById(R.id.miv_chatSettting);
-        miv_personal_info.setImage(R.mipmap.collect);
-        miv_personal_info.setTitle("聊天设置");
-        miv_personal_info.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MCIntent.sendIntentFromAnimLeft(getActivity(), RemindActivity.class);
-            }
-        });
-    }
-    private void initNoDistrub() {
-        MeItemView miv_personal_info = (MeItemView) view.findViewById(R.id.miv_noDistrub);
-        miv_personal_info.setImage(R.mipmap.wallet);
-        miv_personal_info.setTitle("勿扰模式");
-        miv_personal_info.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MCIntent.sendIntentFromAnimLeft(getActivity(), NoDistrubActivity.class);
-            }
-        });
-    }
     private void initPrivate() {
         MeItemView miv_personal_info = (MeItemView) view.findViewById(R.id.miv_private);
-        miv_personal_info.setImage(R.mipmap.card);
+        miv_personal_info.setImage(R.drawable.safe);
         miv_personal_info.setTitle("隐私和安全");
         miv_personal_info.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -127,7 +96,7 @@ public class MeFragment extends Fragment {
     }
     private void initPublic() {
         MeItemView miv_personal_info = (MeItemView) view.findViewById(R.id.miv_public);
-        miv_personal_info.setImage(R.mipmap.look);
+        miv_personal_info.setImage(R.drawable.publicset);
         miv_personal_info.setTitle("通用设置");
         miv_personal_info.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -138,7 +107,7 @@ public class MeFragment extends Fragment {
     }
     private void initTrafficStatistics() {
         MeItemView miv_personal_info = (MeItemView) view.findViewById(R.id.miv_trafficStatistics);
-        miv_personal_info.setImage(R.mipmap.set);
+        miv_personal_info.setImage(R.drawable.statistics);
         miv_personal_info.setTitle("流量统计");
         miv_personal_info.setOnClickListener(new View.OnClickListener() {
             @Override
